3a13dc71fd546ed62db5c47447929d33b86ec486
maiacamarero <maia.camarero@ing.austral.edu.ar>
2023-10-10 17:47:41 -0300

    turn strategy fixed

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -7,19 +7,22 @@ import gameState.InitialGameState
 import gameState.InvalidMovementState
 import gameState.NewGameState
 import turnStrategy.RegularTurnStrategy
+import turnStrategy.TurnStrategy
 import validator.Movement
 import java.util.*
 
-class Engine(private var turnStrategy: RegularTurnStrategy) {
+class Engine() {
 
     private val sc = Scanner(System.`in`)
     private val boardFactory = BoardFactory()
     private val board = boardFactory.createClassicBoard()
     private val movementStrategy = MovementStrategy()
+//    val turnStrategy
 
     fun init() : InitialGameState {
         chooseConfiguration()
-        return InitialGameState(board.getX(), board.getPiecesPositions().values.toList(), PieceColor.WHITE)
+        val turnStrategy : TurnStrategy = RegularTurnStrategy(PieceColor.WHITE)
+        return InitialGameState(board.getX(), board.getPiecesPositions().values.toList(), turnStrategy)
     }
 
     fun getBoard(): Board{
@@ -29,7 +32,9 @@ class Engine(private var turnStrategy: RegularTurnStrategy) {
 //    private var initialColor: PieceColor  EL GAME MANAGER O ALGO ASI DEBERIA SABER DE QUIEN ES EL TURNO
 //    LOS STATES PUEDEN SER INVALID MOVE SI EL MOVIMIENTO ES INVALIDO POR ALGUN MOTIVO O GAME STATE SI ES VALIDO
     fun applyMove(movement: Movement) : GameState {
+
         val pieceToMove : Piece = movement.getPiece()
+        val turnStrategy : TurnStrategy = RegularTurnStrategy(pieceToMove.getPieceColor())
         val fromPosition : Position = board.getPositionByPiece(pieceToMove)
         val toPosition : Position = movement.getFinalPosition()
     return if (pieceToMove.getPieceColor() != turnStrategy.getCurrentColor()){
--- src/main/kotlin/gameState/InitialGameState.kt
+++ src/main/kotlin/gameState/InitialGameState.kt
@@ -2,8 +2,9 @@ package gameState
 
 import piece.Piece
 import piece.PieceColor
+import turnStrategy.TurnStrategy
 
-class InitialGameState(private val boardSize : Int, private val pieces : List<Piece>, private val color : PieceColor) {
+class InitialGameState(private val boardSize : Int, private val pieces : List<Piece>, private val color : TurnStrategy) {
 
     fun getBoardSize() : Int{
         return boardSize
@@ -13,7 +14,7 @@ class InitialGameState(private val boardSize : Int, private val pieces : List<Pi
         return pieces
     }
 
-    fun getColor() : PieceColor{
+    fun getColor() : TurnStrategy{
         return color
     }
 
